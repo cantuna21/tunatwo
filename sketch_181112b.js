53398f62e0a1cec5c061f4a8383f78592c082ddc
cantuna21 <44824517+cantuna21@users.noreply.github.com>
2018-11-13 21:02:11 +0900

    Update sketch_181112b.js

--- sketch_181112b.js
+++ sketch_181112b.js
@@ -1,29 +1,37 @@
 var img;
 var theta = 0;
+var r, g, b;
 
-var speed;
-
-function setup(){
+function setup() {
   createCanvas(1200, 800, WEBGL);
+  r = random(255);
+  g = random(255);
+  b = random(255);
   img = loadImage("asset/dealwithit.jpg");
 }
 
-function draw(){
+function draw() {
   background(0);
+
+  if (mouseIsPressed) {
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    background(r, g, b);
+  }
+
   deal();
 }
 
-function deal(){
-   translate(0,-50,0);
+function deal() {
+  translate(0, -50, 0);
   push();
-    rotateZ(theta * 0.15);
-    rotateX(theta * 0.15);
-    rotateY(theta * 0.15);
-    texture(img);
-    box(300, 300, 300);
+  rotateZ(theta * 0.15);
+  rotateX(theta * 0.15);
+  rotateY(theta * 0.15);
+  texture(img);
+  box(300, 300, 300);
   pop();
-  theta += 0.05;  
-  
-  
+  theta += 0.05;
 }
 
